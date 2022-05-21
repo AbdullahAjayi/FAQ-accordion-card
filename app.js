56b48d4d7bd56d16e34504fc6ee72dc875ad6e77
AbdullahAjayi <hafizajayi2308@gmail.com>
2022-05-21 20:31:44 +0100

    extended js functions on accordion

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 const  accordion =  document.querySelectorAll('.header');
- accordion.forEach(header => header.addEventListener('click', (e) => {
-    
-    header.parentElement.classList.toggle('open');
- } ))
+
+
+ accordion.forEach(header => {
+   header.addEventListener('click', onClick)
+ });
+
+
+ function onClick(e) {
+    accordion.forEach(header => {
+       header.parentElement.classList.remove('open')
+    });
+
+    if(e.target.classList.contains('header-text') || e.target.classList.contains('arrow')) {
+      e.target.parentElement.parentElement.classList.toggle('open');
+    } else {
+       e.target.parentElement.classList.toggle('open');
+    }
+ }
